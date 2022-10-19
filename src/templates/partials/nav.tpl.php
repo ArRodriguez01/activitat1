f95395b0bacd06aa70cd11527d4a3b8210268abd
ArRodriguez01 <a.rodriguez2dawnuria2022@gmail.com>
2022-10-19 17:19:01 +0200

    Eight Commit

--- src/templates/partials/nav.tpl.php
+++ src/templates/partials/nav.tpl.php
@@ -1,19 +1,31 @@
+
 <nav>
-        <?php if(!isset($_SESSION['user'])):?>
+
+<?php if(!isset($_SESSION['user'])):?>
         <a href="?url=login">Login</a>
         <a href="?url=register">Register</a>
         <?php else:?>
         <a href="?url=logout">Logout <?=$_SESSION['user']->uname;?></a>
         <a href="?url=dashboard">Home</a>
-        <label for="dog-names">Choose a dog name:</label>
-<select name="dog-names" id="dog-names">
-    <option value="rigatoni">Rigatoni</option>
-  <option value="dave">Dave</option>
-  <option value="pumpernickel">Pumpernickel</option>
-  <option value="reeses">Reeses</option>
-</select>
-        <?php endif;?>
-        
-        
-        
+        <section class="menu__container">
+        <li class="menu__item menu__item--show">
+                    <div><a href="#" class="menu__link">User<img src="../../../public/assets/arrow.svg" class="menu__arrow"></a></div>
+    
+                    <ul class="menu__nesting">
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">Settings</a>
+                        </li>
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">About 2</a>
+                        </li>
+                        <li class="menu__inside">
+                            <a href="#" class="menu__link menu__link--inside">About 3</a>
+                        </li>
+                    </ul>
+        <div class="menu__hamburguer">
+                <img src="assets/menu.svg" class="menu__img">
+            </div>
+        </li>
+        </section>
+        <?php endif;?>    
 </nav>
